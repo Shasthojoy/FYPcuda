7d50cd7a558c4a9a41029b2b76552e001c826a36
ACERLAP\Mendo <mendozaflash66@outlook.com>
2016-11-19 01:10:43 +0530

    Cuda implementation

--- Project/cudaloading.cu
+++ Project/cudaloading.cu
@@ -2,17 +2,23 @@
 #include "device_launch_parameters.h"
 #include "cuda_runtime.h"
 
-cudaloading::cudaloading(const readmat& MAT)
+cudaloading::cudaloading(const readmat& MAT,int alpha)
 {
-	thisData.elements = MAT.getarraypointer;
-	dimensionpointer = MAT.dimensionpointer;
+	thisData.elements = MAT.getarraypointer();
+	dimensionpointer = MAT.dimensionpointer();
 	thisData.X = *(dimensionpointer);
 	thisData.Y = *(dimensionpointer + 1);
 	thisData.U = *(dimensionpointer + 2);
 	thisData.V = *(dimensionpointer + 3);
+	thisData.alpha = alpha;
+
+
 
 }
 
+void cudaloading::cudacopy()
+{
 
+}
 
 
--- Project/cudaloading.cuh
+++ Project/cudaloading.cuh
@@ -1,23 +1,30 @@
 #ifndef CUDALOADING_H
 #define CUDALOADING_H
+#include "device_launch_parameters.h"
+#include "cuda_runtime.h"
 #include"readmat.h"
+
 typedef struct {
 	size_t X;
 	size_t Y;
 	size_t U;
 	size_t V;
 	double* elements;
+	int no_of_elements;
+	int alpha;
 } DataIn;
 
 class cudaloading
 {
 private:
 	//readmat thisMat;
-	
+	const size_t* dimensionpointer;
 	DataIn thisData;
+	double* device_elements;
+	cudaError_t cudaStatus;
 
 public:
-	cudaloading(const readmat& MAT);
+	cudaloading(const readmat& MAT, int alpha);
 	void cudaprepareInt();
 	void cudacopy();
 
